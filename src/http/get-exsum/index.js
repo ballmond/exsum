2762c6ea96c6ffe949b05f0afcaa8becfde36cbb
Robert Allmond <ballmond@gmail.com>
2020-12-20 16:14:00 -0500

    update vars

--- src/http/get-exsum/index.js
+++ src/http/get-exsum/index.js
@@ -6,17 +6,20 @@ exports.handler = async function http (req) {
   var params = {
       Bucket: `exsum-reports/${study}`,
       Key: `${study}.xlsm`
-  };
+    };
   
-  AWS.config.update({
-    region: "us-east-1"
-  })
-  console.log(process.env.ACCESS_KEY_ID)
-  const s3 = new AWS.S3()
-  const myBucket = params.Bucket
-  const myKey = params.Key
-  const signedUrlExpireSeconds = 60 * 5
-  const url = s3.getSignedUrl('getObject', {
+    AWS.config.update({
+      accessKeyId: process.env.ACCESS_KEY_ID, 
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      region: "us-east-1"
+    })
+    console.log(process.env.ACCESS_KEY_ID)
+    const s3 = new AWS.S3()
+    const myBucket = params.Bucket
+    const myKey = params.Key
+    const signedUrlExpireSeconds = 60 * 5
+  
+    const url = s3.getSignedUrl('getObject', {
       Bucket: myBucket,
       Key: myKey,
       Expires: signedUrlExpireSeconds
